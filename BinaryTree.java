13de12482f82d8bc0ce4657da3b241868daacc6d
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-08-12 23:17:27 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -28,6 +28,7 @@ public class BinaryTree{
         }
     }
     public static void main(String []args){
+        
         int nodes[]  = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BiTree tree = new BiTree();
         Node root = tree.buildTree(nodes);
